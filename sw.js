675beb811eb88cff1e6375f60f3fde18aaf5205b
ROCGMC <a0960730188@gmail.com>
2026-02-23 02:56:46 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-const CACHE_NAME = 'mdm-v1';
+const CACHE_NAME = 'ezert-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png'
+  '/Ezert/',
+  '/Ezert/index.html',
+  '/Ezert/manifest.json',
+  '/Ezert/icon.png'
 ];
 
-// 安裝時儲存檔案到快取
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
@@ -14,16 +13,6 @@ self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-// 啟用時清理舊快取
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
-});
-
-// 攔截請求，優先使用快取（實現離線瀏覽）
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
