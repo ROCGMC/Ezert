5e4061d8c3098cbd9bdbc94b921f4fc6378b65bc
ROCGMC <a0960730188@gmail.com>
2026-02-23 02:52:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// sw.js 內容範例
 const CACHE_NAME = 'mdm-v1';
 const ASSETS = [
   './',
@@ -7,14 +6,24 @@ const ASSETS = [
   './icon.png'
 ];
 
-// 1. 安裝時快取資源
+// 安裝時儲存檔案到快取
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-// 2. 攔截請求（這是安裝按鈕出現的關鍵）
+// 啟用時清理舊快取
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+});
+
+// 攔截請求，優先使用快取（實現離線瀏覽）
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
