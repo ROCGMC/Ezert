f99d753e30265f63262f4b125ccf404d1272da72
ROCGMC <a0960730188@gmail.com>
2026-02-23 09:05:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mdm-v2'; // 修改這裡的版本號，強制刷新
+const CACHE_NAME = 'mdm-v2';
 const assets = [
   './',
   './index.html',
@@ -9,6 +9,14 @@ const assets = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
 });
 
