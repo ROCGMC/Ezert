2cfaf1421f6562fe61aac8583ef7ffe2d26f83bd
ROCGMC <a0960730188@gmail.com>
2026-02-23 11:07:34 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'mdm-cache-v1'; // 更改此版本號可強制更新全體客戶端
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// 安裝並快取資源
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('正在快取國軍 MDM 資源');
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); // 強制進入 activate 階段
+});
+
+// 清理舊版快取
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('清理舊快取:', key);
+          return caches.delete(key);
+        }
+      })
+    ))
+  );
+  return self.clients.claim();
+});
+
+// 攔截請求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
