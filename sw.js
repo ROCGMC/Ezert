f94e22bad2d2140d7308ed4ffcd35a7dd6f9cbef
ROCGMC <a0960730188@gmail.com>
2026-02-23 12:03:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,17 @@
-const CACHE_NAME = 'mdm-cache-v3'; // 更改此版本號可強制更新全體客戶端
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png'
-];
+const CACHE_NAME = 'mdm-final-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
 
-// 安裝並快取資源
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('正在快取國軍 MDM 資源');
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting(); // 強制進入 activate 階段
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-// 清理舊版快取
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE_NAME) {
-          console.log('清理舊快取:', key);
-          return caches.delete(key);
-        }
-      })
-    ))
-  );
-  return self.clients.claim();
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.map(k => k !== CACHE_NAME && caches.delete(k))
+  )));
 });
 
-// 攔截請求
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
